8a7aade8a9a3033cfd2086a709acc79d1a681623
LoadingGod <jens.mekers@hotmail.com>
2015-10-06 10:57:51 +0200

    1

--- src/Hoofdstuk_5/Oef_4.java
+++ src/Hoofdstuk_5/Oef_4.java
@@ -6,6 +6,14 @@ public class Oef_4 {
 	private static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
+		int tijd = input.nextInt();
+		int uren;
+		int minuten;
+		int seconden;
+		uren = tijd / 3600;
+		minuten = (tijd - (uren *3600))/60;
+		seconden = tijd - (uren *3600) - (minuten *60);
+		System.out.println(uren + " "+ minuten + " "+ seconden);
+		
 	}
 }
